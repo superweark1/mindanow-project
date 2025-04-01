1f7ae8db218ee725f098af061c517b2f89878333
superweark1 <earlsonnieandrew.plecerda1@gmail.com>
2025-04-01 18:59:41 +0800

    updated

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+const config = {
+    API_URL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+    APP_NAME: import.meta.env.VITE_APP_NAME || "My Vite App",
+  };
+  
+  export default config;
+  
